8a0813c7bb824f41bdbdf9613b882cf2a251b4a0
Rino <rinooktavianridwan@gmail.com>
2025-03-05 21:29:01 +0700

    add data dummy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Toko;
+use App\Models\Produk;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +17,55 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Toko::create([
+            'nama_toko' => 'Toko Jaya Makmur',
+            'pemilik' =>  'Budi',
+            'alamat' =>  'Jl. Superman No. 45, Jakarta',
+            'email' =>  'warungmaju@gmail.com',
+            'no_telepon' =>  '082345678901'
+        ]);
+
+        Toko::create([
+            'nama_toko' => 'Toko Sumber Murah',
+            'pemilik' =>  'Suryono Jaya',
+            'alamat' =>  'Jl. Merdeka No. 10, Jakarta Pusat',
+            'email' =>  'suryonojaya@gmail.com',
+            'no_telepon' =>  '082345671234'
+        ]);
+
+        Produk::create([
+            'toko_id' => 1,
+            'nama_produk' => 'Gula Aren',
+            'harga' => 5000,
+            'stok' => 50
+        ]);
+
+        Produk::create([
+            'toko_id' => 1,
+            'nama_produk' => 'Beras',
+            'harga' => 75000,
+            'stok' => 5
+        ]);
+
+        Produk::create([
+            'toko_id' => 2,
+            'nama_produk' => 'Kopi Hitam',
+            'harga' => 10000,
+            'stok' => 30
+        ]);
+
+        Produk::create([
+            'toko_id' => 2,
+            'nama_produk' => 'Teh Hijau',
+            'harga' => 8000,
+            'stok' => 40
+        ]);
+
+        Produk::create([
+            'toko_id' => 2,
+            'nama_produk' => 'Jahe Merah',
+            'harga' => 12000,
+            'stok' => 20
         ]);
     }
 }
